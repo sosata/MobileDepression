ab44ce423767178b637aa5cc8ed5ded6042d89dc
sosata <sosata@gmail.com>
2016-12-08 17:56:30 -0600

    more sensors

--- lstm.py
+++ lstm.py
@@ -1,17 +1,21 @@
 
 # coding: utf-8
 
-# In[19]:
+# In[130]:
 
 import pandas as pd
 import numpy as np
 
 data_fus = pd.read_csv('/home/sohrob/Dropbox/Data/CS120/1022235/fus.csv',sep='\t',header=None)
 data_coe = pd.read_csv('/home/sohrob/Dropbox/Data/CS120/1022235/coe.csv',sep='\t',header=None)
+data_act = pd.read_csv('/home/sohrob/Dropbox/Data/CS120/1022235/act.csv',sep='\t',header=None)
+data_scr = pd.read_csv('/home/sohrob/Dropbox/Data/CS120/1022235/scr.csv',sep='\t',header=None)
+data_aud = pd.read_csv('/home/sohrob/Dropbox/Data/CS120/1022235/aud.csv',sep='\t',header=None)
+data_cal = pd.read_csv('/home/sohrob/Dropbox/Data/CS120/1022235/cal.csv',sep='\t',header=None)
 target = pd.read_csv('/home/sohrob/Dropbox/Data/CS120/1022235/emm.csv',sep='\t',header=None)
 
 
-# In[20]:
+# In[132]:
 
 # adapting data to neural net
 
@@ -19,15 +23,17 @@ deltat = 600
 win = 12*3600
 
 for (i,t1) in enumerate(target[0]):
+    lat = np.nan
+    lng = np.nan
     for t2 in np.arange(t1-win, t1, deltat):
         # capturing gps data
         ind = np.where(data_fus[0].between(t2, t2+deltat, inclusive=True))[0]
         if ind.size:
             lat = np.nanmean(data_fus[1][ind])
             lng = np.nanmean(data_fus[2][ind])
-        else:
-            lat = np.nan
-            lng = np.nan
+#         else:
+            # nothing, just keep the last one
+            
         # capturing communication data
         ind = np.where(data_coe[0].between(t2, t2+deltat, inclusive=True))[0]
         if ind.size:
@@ -42,12 +48,58 @@ for (i,t1) in enumerate(target[0]):
             incoming = 0
             outgoing = 0
             missed = 0
+            
+        # activity data
+        ind = np.where(data_act[0].between(t2, t2+deltat, inclusive=True))[0]
+        if ind.size:
+            act_onfoot = np.sum(data_act[1][ind]=='ON_FOOT')/float(ind.size)
+            act_still = np.sum(data_act[1][ind]=='STILL')/float(ind.size)
+            act_invehicle = np.sum(data_act[1][ind]=='IN_VEHICLE')/float(ind.size)
+            act_tilting = np.sum(data_act[1][ind]=='TILTING')/float(ind.size)
+            act_confidence = np.nanmean(data_act[2][ind])
+        else:
+            act_onfoot = 0
+            act_still = 1
+            act_invehicle = 0
+            act_tilting = 0
+            act_confidence = 100
+            
+        # screen data
+        ind = np.where(data_scr[0].between(t2, t2+deltat, inclusive=True))[0]
+        if ind.size:
+            scr_n = ind.size/2.0
+        else:
+            scr_n = 0
+        
+        # audio data
+        ind = np.where(data_aud[0].between(t2, t2+deltat, inclusive=True))[0]
+        if ind.size:
+            aud_amp = np.mean(data_aud[2][ind])
+            aud_frq = np.mean(data_aud[3][ind])
+        else:
+            aud_amp = -10.0
+            aud_frq = -100.0
+
+        # call data
+        ind = np.where(data_cal[0].between(t2, t2+deltat, inclusive=True))[0]
+        if ind.size:
+            cal_dur = np.sum(data_cal[1][ind]=='Off-Hook')
+        else:
+            cal_dur = 0
+
+        # time
         hour = np.mod(t1,86400)/3600.0
         dow = np.mod(t1,86400*7)/86400.0
-        stress = target.loc[i,2]
-
-        ft = np.array([lat, lng, hour, dow, stress, sms, phone, incoming, outgoing, missed])
+        
+#         stress = target.loc[i,2]
+        
+        # input vector
+#         ft = np.array([lat, lng, hour, dow, stress, sms, phone, incoming, outgoing, missed, act_onfoot,\
+#                       act_still, act_invehicle, act_tilting, act_confidence])
+        ft = np.array([lat, lng, hour, dow, sms, phone, incoming, outgoing, missed, act_onfoot,                      act_still, act_invehicle, act_tilting, act_confidence,                      scr_n,                      aud_amp, aud_frq,                      cal_dur])
         ft = ft.reshape(1,ft.size)
+        
+        # adding to input matrix
         if t2==t1-win:
             x_sample = ft
         else:
@@ -64,9 +116,10 @@ for (i,t1) in enumerate(target[0]):
         y = np.append(y, mood, axis=0)
 
 
-# In[21]:
+# In[133]:
 
 # remove samples that contain nan
+
 ind_del = []
 for i in range(x.shape[0]):
     if np.sum(np.sum(np.isnan(x[i,:,:])))>0:
@@ -75,27 +128,28 @@ x = np.delete(x, ind_del, axis=0)
 y = np.delete(y, ind_del, axis=0)
 
 
-# In[28]:
+# In[158]:
 
-# building the network
+# build the network
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, LSTM, TimeDistributed
 from keras import regularizers, optimizers
 
 reg = regularizers.WeightRegularizer(l1=0, l2=.1)
-optim = optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+optim = optimizers.Adam(lr=0.0003, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 
 model = Sequential()
-model.add(LSTM(output_dim=20, input_dim=x.shape[2], return_sequences=False, activation='tanh', W_regularizer=reg,              dropout_W=0.5,dropout_U=0.5))
-model.add(Dense(20, activation='tanh'))
+model.add(LSTM(output_dim=100, input_dim=x.shape[2], return_sequences=False, activation='tanh', W_regularizer=reg,              dropout_W=0.5,dropout_U=0.5))
+# model.add(Dense(20, activation='tanh'))
+model.add(Dense(50, activation='tanh'))
 model.add(Dense(y.shape[1], activation='linear'))
 # model.add(TimeDistributed(Dense(output_dim=1)))
 
 model.compile(optimizer=optim, loss='mse')
 
 
-# In[31]:
+# In[159]:
 
 # training
 
@@ -111,10 +165,10 @@ x_test = x_test - np.ones([x_test.shape[0],x_test.shape[1],1])*np.mean(np.mean(x
 
 # y = y - np.mean(y)
 
-model.fit(x_train, y_train, batch_size=1, verbose=1, nb_epoch=30, validation_data=(x_test,y_test))
+model.fit(x_train, y_train, batch_size=1, verbose=1, nb_epoch=50, validation_data=(x_test,y_test))
 
 
-# In[32]:
+# In[160]:
 
 # prediction
 
@@ -127,15 +181,17 @@ y_pred = model.predict(x_test)
 plt.figure(figsize=[5,2])
 plt.plot(y_train, color=(0,.5,0))
 plt.plot(y_pred_train, color=(0,0,0))
-plt.text(y_train.shape[0],6,'mse=%.2f' % np.mean(np.power(y_pred_train-y_train,2)))
+plt.text(y_train.shape[0],max(y_train)+.5,'R2=%.2f' % (1-np.sum(np.power(y_pred_train-y_train,2))/np.sum(np.power(np.mean(y_train)-y_train,2))))
+plt.title('train')
 
 plt.figure(figsize=[5,2])
 plt.plot(y_test, color=(0,.5,0))
 plt.plot(y_pred, color=(0,0,0))
-plt.text(y_test.shape[0],6,'mse=%.2f' % np.mean(np.power(y_pred-y_test,2)))
+plt.text(y_test.shape[0],max(y_test)+.5,'R2=%.2f' % (1-np.sum(np.power(y_pred-y_test,2))/np.sum(np.power(np.mean(y_test)-y_test,2))))
+plt.title('test')
 
 
-# In[18]:
+# In[33]:
 
-x[:,0,-1]
+x.shape
 
