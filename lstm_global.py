bb9324f59618c38bd1b1211bffbd766b27d894c3
sosata <sosata@gmail.com>
2016-12-16 17:25:35 -0600

    global model train/test

--- lstm_global.py
+++ lstm_global.py
@@ -1,188 +1,31 @@
 
 # coding: utf-8
 
-# In[12]:
+# In[14]:
 
 import pandas as pd
 import numpy as np
 from scipy import stats
-import os
+import pickle
 
-deltat = 1800
-win = 24*3600
-
-data_dir = '/home/sohrob/Dropbox/Data/CS120/'
-subjects = os.listdir(data_dir)
-
-# subjects = subjects[:10]
-
-for (s,subject) in enumerate(subjects):
-    
-    print s,
-    if os.path.exists(data_dir+subject+'/act.csv'):
-        data_act = pd.read_csv(data_dir+subject+'/act.csv',sep='\t',header=None)
-    else:
-        print ' skipping - no data'
-        continue
-    if os.path.exists(data_dir+subject+'/aud.csv'):
-        data_aud = pd.read_csv(data_dir+subject+'/aud.csv',sep='\t',header=None)
-    else:
-        print ' skipping - no data'
-        continue
-    if os.path.exists(data_dir+subject+'/bat.csv'):
-        data_bat = pd.read_csv(data_dir+subject+'/bat.csv',sep='\t',header=None)
-    else:
-        print ' skipping - no data'
-        continue
-    if os.path.exists(data_dir+subject+'/cal.csv'):
-        data_cal = pd.read_csv(data_dir+subject+'/cal.csv',sep='\t',header=None)
-    else:
-        print ' skipping - no data'
-        continue
-    if os.path.exists(data_dir+subject+'/coe.csv'):
-        data_coe = pd.read_csv(data_dir+subject+'/coe.csv',sep='\t',header=None)
-    else:
-        print ' skipping - no data'
-        continue
-    if os.path.exists(data_dir+subject+'/fus.csv'):
-        data_fus = pd.read_csv(data_dir+subject+'/fus.csv',sep='\t',header=None)
-    else:
-        print ' skipping - no data'
-        continue
-    if os.path.exists(data_dir+subject+'/scr.csv'):
-        data_scr = pd.read_csv(data_dir+subject+'/scr.csv',sep='\t',header=None)
-    else:
-        print ' skipping - no data'
-        continue
-    if os.path.exists(data_dir+subject+'/wif.csv'):
-        data_wif = pd.read_csv(data_dir+subject+'/wif.csv',sep='\t',header=None)
-    else:
-        print ' skipping - no data'
-        continue
-    if os.path.exists(data_dir+subject+'/emm.csv'):
-        target = pd.read_csv(data_dir+subject+'/emm.csv',sep='\t',header=None)
-    else:
-        print ' skipping - no data'
-        continue
-    print
-
-    for (i,t1) in enumerate(target[0]):
-        lat = np.nan
-        lng = np.nan
-        for t2 in np.arange(t1-win, t1, deltat):
-
-            # GPS data
-            ind = np.where(data_fus[0].between(t2, t2+deltat, inclusive=True))[0]
-            if ind.size:
-                lat = np.nanmean(data_fus[1][ind])
-                lng = np.nanmean(data_fus[2][ind])
-    #         else:
-                # nothing, just keep the last one
-
-            # communication data
-            ind = np.where(data_coe[0].between(t2, t2+deltat, inclusive=True))[0]
-            if ind.size:
-                sms = np.sum(data_coe[3][ind]=='SMS')
-                phone = np.sum(data_coe[3][ind]=='PHONE')
-                incoming = np.sum(data_coe[4][ind]=='INCOMING')
-                outgoing = np.sum(data_coe[4][ind]=='OUTGOING')
-                missed = np.sum(data_coe[4][ind]=='MISSED')
-            else:
-                sms = 0
-                phone = 0
-                incoming = 0
-                outgoing = 0
-                missed = 0
-
-            # activity data
-            ind = np.where(data_act[0].between(t2, t2+deltat, inclusive=True))[0]
-            if ind.size:
-                act_onfoot = np.sum(data_act[1][ind]=='ON_FOOT')/float(ind.size)
-                act_still = np.sum(data_act[1][ind]=='STILL')/float(ind.size)
-                act_invehicle = np.sum(data_act[1][ind]=='IN_VEHICLE')/float(ind.size)
-                act_tilting = np.sum(data_act[1][ind]=='TILTING')/float(ind.size)
-                act_confidence = np.nanmean(data_act[2][ind])
-            else:
-                act_onfoot = 0
-                act_still = 1
-                act_invehicle = 0
-                act_tilting = 0
-                act_confidence = 100
-
-            # screen data
-            ind = np.where(data_scr[0].between(t2, t2+deltat, inclusive=True))[0]
-            if ind.size:
-                scr_n = ind.size/2.0
-            else:
-                scr_n = 0
-
-            # audio data
-            ind = np.where(data_aud[0].between(t2, t2+deltat, inclusive=True))[0]
-            if ind.size:
-                aud_amp = np.mean(data_aud[2][ind])
-                aud_frq = np.mean(data_aud[3][ind])
-            else:
-                aud_amp = 0
-                aud_frq = 0
-
-            # call data
-            ind = np.where(data_cal[0].between(t2, t2+deltat, inclusive=True))[0]
-            if ind.size:
-                cal_dur = np.sum(data_cal[1][ind]=='Off-Hook')
-            else:
-                cal_dur = 0
-
-            # battery data
-            ind = np.where(data_bat[0].between(t2, t2+deltat, inclusive=True))[0]
-            if ind.size:
-                bat_charge = np.mean(data_bat[1][ind])
-                bat_state = stats.mode(data_bat[2][ind])[0][0]
-            else:
-                bat_charge = np.nan
-                bat_state = np.nan
-
-            # wifi data
-            ind = np.where(data_wif[0].between(t2, t2+deltat, inclusive=True))[0]
-            if ind.size:
-                wif_n = np.mean(data_wif[3][ind])
-            else:
-                wif_n = np.nan
-
-            # time
-            hour = np.mod(t1,86400)/3600.0
-            dow = np.mod(t1,86400*7)/86400.0
-
-    #         stress = target.loc[i,2]
-
-            # input vector
-            ft = np.array([lat, lng,                           hour, dow,                           sms, phone, incoming, outgoing, missed,                           act_onfoot, act_still, act_invehicle, act_tilting, act_confidence,                           scr_n,                           aud_amp, aud_frq,                           cal_dur,                           bat_charge, bat_state,                           wif_n])
-
-            ft = ft.reshape(1,ft.size)
-
-            # adding to input matrix
-            if t2==t1-win:
-                x_sample = ft
-            else:
-                x_sample = np.append(x_sample, ft, axis=0)
-
-        mood = target.loc[i,1]
-        mood = mood.reshape(1,mood.size)
-
-        try:
-            x = np.append(x, x_sample.reshape(1,x_sample.shape[0],x_sample.shape[1]), axis=0)
-            y = np.append(y, mood, axis=0)
-        except:
-            x = x_sample.reshape(1,x_sample.shape[0],x_sample.shape[1])
-            y = mood
-            
+with open('data_lstm.dat') as f:
+    data = pickle.load(f)
+f.close()
 
 
-# In[ ]:
+# In[18]:
 
-target
+# concatenating sensor and target data
+
+n_core = 12
+x = data[0]
+y = data[1]
+for i in range(n_core-1):
+    x = np.concatenate([x,data[(i+1)*2]], axis=0)
+    y = np.concatenate([y,data[(i+1)*2+1]], axis=0)
 
 
-# In[4]:
+# In[21]:
 
 # remove samples that contain nan
 
@@ -195,7 +38,7 @@ x = np.delete(x, ind_del, axis=0)
 y = np.delete(y, ind_del, axis=0)
 
 
-# In[9]:
+# In[30]:
 
 # build the network
 
@@ -212,23 +55,23 @@ model = Sequential()
 model.add(LSTM(output_dim=100, input_dim=x.shape[2], return_sequences=False, activation='tanh', W_regularizer=reg,              dropout_W=0.2, dropout_U=0.2))
 # model.add(Dense(20, activation='tanh'))
 model.add(Dense(50, activation='tanh'))
-model.add(Dense(y.shape[1], activation='linear'))
+model.add(Dense(1, activation='linear'))
 # model.add(TimeDistributed(Dense(output_dim=1)))
 
 model.compile(optimizer=optim, loss='mse')
 
 
-# In[10]:
+# In[37]:
 
 # training
 
-split = int(round(x.shape[0]*0.8))
+split = int(round(x.shape[0]*0.9))
 
 x_train = x[:split,:,:]
-y_train = y[:split,:]
+y_train = y[:split,1] # only mood
 
 x_test = x[split:,:,:]
-y_test = y[split:,:]
+y_test = y[split:,1] # only mood
 
 # centering the input
 x_train = x_train - np.ones([x_train.shape[0],x_train.shape[1],1])*np.mean(np.mean(x_train,axis=0),axis=0)
@@ -236,24 +79,24 @@ x_test = x_test - np.ones([x_test.shape[0],x_test.shape[1],1])*np.mean(np.mean(x
 
 # y = y - np.mean(y)
 
-model.fit(x_train, y_train, batch_size=10, verbose=1, nb_epoch=20, validation_data=(x_test,y_test))
+model.fit(x_train, y_train, batch_size=1000, verbose=1, nb_epoch=20, validation_data=(x_test,y_test))
 
 
-# In[11]:
+# In[38]:
 
 # prediction
 
 import matplotlib.pyplot as plt
 get_ipython().magic(u'matplotlib inline')
 
-y_pred_train = model.predict(x_train)
-y_pred = model.predict(x_test)
+y_pred_train = model.predict(x_train).reshape([y_train.shape[0],])
+y_pred = model.predict(x_test).reshape([y_test.shape[0],])
 
 plt.figure(figsize=[10,2])
 plt.plot(y_train, color=(0,.5,0))
 plt.plot(y_pred_train, color=(0,0,0))
 plt.xlim([0, y_train.shape[0]-1])
-plt.ylim([0, 9])
+plt.ylim([-1, 10])
 plt.text(y_train.shape[0],9,'R2=%.2f' % (1-np.sum(np.power(y_pred_train-y_train,2))/np.sum(np.power(np.mean(y_train)-y_train,2))))
 plt.title('train')
 
@@ -261,7 +104,12 @@ plt.figure(figsize=[10,2])
 plt.plot(y_test, color=(0,.5,0))
 plt.plot(y_pred, color=(0,0,0))
 plt.xlim([0, y_test.shape[0]-1])
-plt.ylim([0, 9])
+plt.ylim([-1, 10])
 plt.text(y_test.shape[0],9,'R2=%.2f' % (1-np.sum(np.power(y_pred-y_test,2))/np.sum(np.power(np.mean(y_train)-y_test,2))))
 plt.title('test')
+
+
+# In[35]:
+
+y_test.shape
 
